bca1c966c6ea6ce9ca89b5bd250a437d00c836df
Ramtin Hasanzadeh <148252584+Ramtindoc@users.noreply.github.com>
2025-08-06 13:22:52 +0330

    Update reverse_strings.py

--- reverse_strings.py
+++ reverse_strings.py
@@ -3,8 +3,9 @@ def reversed_text(text): return text[::-1]
 result = reversed_text("text")
 # print(result)
 
-# اگر بخوایم ورودی از کار کاربر داشته باشیم :
+# اگر بخوایم ورودی از کاربر داشته باشیم :
 # حتی اگر از نوع عدد هم باشه به رشته تبدیل میشه و ریورس میشه
 user_input = str(input("Enter a string to reverse it:"))
 result = reversed_text(user_input)
+
 # print("result: ",result)
