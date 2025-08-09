4558b6cb23243dff7d3b84d1969f798e11fb379f
Ramtin Hasanzadeh <148252584+Ramtindoc@users.noreply.github.com>
2025-08-09 11:09:01 +0330

    Update create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -1,5 +1,5 @@
 import csv
-
+# exm data
 sales_data = [
     ['names', 'sales'],
     ['ali', 12000000],
@@ -12,5 +12,6 @@ sales_data = [
 with open('csv/sales.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerows(sales_data)
+
 
 print("sales.csv created")
